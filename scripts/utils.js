b5af24086d55792f4d25a917c52c82efbdca3a47
ihtfy <oeluer@gmail.com>
2023-07-28 16:10:07 -0700

    flag random type

--- scripts/utils.js
+++ scripts/utils.js
@@ -13,7 +13,7 @@ const getDateSeed = () => {
  * @returns
  */
 const mulberry32 = (a) => {
-  return function () {
+  return () => {
     let t = (a += 0x6d2b79f5);
     t = Math.imul(t ^ (t >>> 15), t | 1);
     t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
@@ -23,15 +23,16 @@ const mulberry32 = (a) => {
 
 /* Overwrite Math.random with a seeded PRNG */
 const dateRandom = mulberry32(getDateSeed());
-const oldRandom = Math.random; // Save the original Math.random for unlimited puzzles
-Math.random = dateRandom;
+// const oldRandom = Math.random; // Save the original Math.random for unlimited puzzles
+// Math.random = dateRandom;
 
 /**
  * Get a random element from an array
  * @param {[]}} arr Any array
  * @returns {*} A random element from the array
  */
-const pickRandom = (arr) => arr.at(Math.floor(Math.random() * arr.length));
+const pickRandom = (arr, seeded = true) =>
+  arr.at(Math.floor((seeded ? dateRandom() : Math.random()) * arr.length));
 
 /**
  * Shuffle an array
@@ -39,10 +40,10 @@ const pickRandom = (arr) => arr.at(Math.floor(Math.random() * arr.length));
  * @param {boolean} inplace If true, the array will be shuffled in place
  * @returns {[]} The shuffled array
  */
-const shuffle = (a, inplace = false) => {
+const shuffle = (a, inplace = false, seeded = true) => {
   let arr = inplace ? a : [...a];
   for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor((seeded ? dateRandom() : Math.random()) * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
