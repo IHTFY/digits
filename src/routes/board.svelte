a551cf40a308b8074b895cad34d92abc4b367128
ihtfy <oeluer@gmail.com>
2023-07-31 21:54:59 -0700

    local storage

--- src/routes/board.svelte
+++ src/routes/board.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { currentPuzzleIndex, puzzleData } from '$lib/stores';
+	import { getDateSeed } from '$lib/utils';
+	import { onMount } from 'svelte';
 	import { cubicOut } from 'svelte/easing';
 	import { tweened } from 'svelte/motion';
 	import Operations from './operations.svelte';
@@ -7,17 +9,36 @@
 	import PuzzleTab from './puzzleTab.svelte';
 
 	const totalStars = tweened(0, {
-		duration: 400,
+		duration: 300,
 		easing: cubicOut
 	});
 
 	let level = 'Beginner';
+	const dateSeed = getDateSeed();
+	let todayData;
+	/**
+	 * @type {() => void}
+	 */
+	let saveData = () => {};
+
+	onMount(() => {
+		const loadedData = window.localStorage.getItem(String(dateSeed));
+		if (loadedData) puzzleData.set(JSON.parse(loadedData));
+
+		saveData = () => {
+			window.localStorage.setItem(String(dateSeed), JSON.stringify($puzzleData));
+			// window.localStorage.setItem('currentPuzzleIndex', JSON.stringify($currentPuzzleIndex));
+		};
+	});
+
 	$: {
 		totalStars.set($puzzleData.reduce((a, c) => a + c.stars, 0));
 
 		level = ['Beginner', 'Moving Up', 'Solid', 'Nice', 'Great', 'Amazing', 'Genius'][
-			Math.floor(Math.max($totalStars - 1, 0) / 3)
+			Math.round($totalStars === 15 ? 16 : $totalStars / 3)
 		];
+
+		saveData();
 	}
 </script>
 
@@ -37,7 +58,7 @@
 				/>
 			{/each}
 		</table>
-		<span>{level}</span>
+		<ins>{level}</ins>
 		<progress value={$totalStars} max="15" />
 		<Operations />
 	</div>
