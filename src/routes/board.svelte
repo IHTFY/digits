a9cd2b5ece04ce42adbe649418f377df5c4bc807
ihtfy <oeluer@gmail.com>
2023-08-01 01:34:54 -0700

    fix level

--- src/routes/board.svelte
+++ src/routes/board.svelte
@@ -35,7 +35,7 @@
 		totalStars.set($puzzleData.reduce((a, c) => a + c.stars, 0));
 
 		level = ['Beginner', 'Moving Up', 'Solid', 'Nice', 'Great', 'Amazing', 'Genius'][
-			Math.round(($totalStars === 15 ? 16 : $totalStars) / 3)
+			$totalStars === 15 ? 6 : $totalStars === 14 ? 5 : Math.floor($totalStars / 3)
 		];
 
 		saveData();
