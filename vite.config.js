3728a313790372025badd9c9ee03edefef56ecc9
ihtfy <13478274+IHTFY@users.noreply.github.com>
2024-10-16 12:54:15 -0700

    manual setup instead of vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		VitePWA({
-			manifest: {
-				workbox: {
-					globPatterns: ['**/*']
-				},
-				includeAssets: ['**/*'],
-				name: 'Digits',
-				short_name: 'Digits',
-				icons: [
-					{
-						src: '/favicons/android-chrome-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: '/favicons/android-chrome-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				],
-				theme_color: '#1095c1',
-				background_color: '#ffffff',
-				display: 'standalone',
-				start_url: '/index.html'
-			}
-		})
-	]
+	plugins: [sveltekit()]
 });
